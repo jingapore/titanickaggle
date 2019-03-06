def973b0c572bc1ac90c86a94af51e9ff4fcccb7
jingapore <ngjingsong@gmail.com>
2019-03-07 00:47:21 +0800

    Analysed 'Cabin' feature. Generated correlation table.

--- titanic.py
+++ titanic.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re, os
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -19,20 +20,18 @@ Remove examples with null vals from 'Age' and 'Embarked'.
 
 traindf = pd.read_csv('./TitanicData/all/train.csv').round({'Age':0}).drop(columns=['PassengerId', 'Ticket'])
 traindf.dropna(axis=0, subset=['Age'], inplace=True)
-# print('Length and na count in traindf')
-# print(len(traindf))
-# print(traindf.isna().sum())
 
 testdf = pd.read_csv('./TitanicData/all/test.csv').round({'Age':0}).drop(columns=['PassengerId', 'Ticket'])
-# print('Length and na count in testdf')
-# print(len(testdf))
-# print(testdf.isna().sum())
 testdf.dropna(axis=0, subset=['Age'], inplace=True) #331 rows
+testdf['Train'] = 0 #To identify whether example is from training set or test set, after concat.
 
 train_survival = traindf['Survived']
 
 traindf.drop(labels='Survived', axis=1, inplace=True)
+traindf['Train'] = 1 #To identify whether example is from training set or test set, after concat.
+
 combineddf = pd.concat([traindf, testdf], ignore_index=True) #1043 rows, of which 712 are training data and 331 are test data.
+combineddf['Sex'] = combineddf['Sex'].apply(lambda x: 1 if x=='male' else 0) #1 for male, 2 for female.
 
 #samplesub = pd.read_csv('./TitanicData/all/gender_submission.csv')
 
@@ -55,32 +54,32 @@ combineddf_nocabin = combineddf[no_cabin_mask]
 #Function with input of 'Cabin' values for passenger, and return number of cabins for passenger.
 def countCabin(x):
     if isinstance(x, str):
-        return x.count(' ')+1
+        # To count 'F GXX' as 1, instead of 2--which "x.count(' ')+1" would.
+        return len(re.findall(pattern=r'\d+\s+', string=x))
     else:
         return 0
 
 combineddf['CabinCount'] = combineddf['Cabin'].apply(countCabin)
 
+#To plot stacked graph of 'CabinCount' vs 'Pclass'.
 Pclass_CabinCount_group = combineddf.groupby(['Pclass', 'CabinCount']).size().unstack('CabinCount').fillna(0)
+ax_CabinCountvsPclass = Pclass_CabinCount_group.plot(kind='bar', stacked=True, colormap='Blues', edgecolor='black')
 
-sns.plot(Pclass_CabinCount_group)
-ax_CabinCountvsPclass = Pclass_CabinCount_group.plot(kind='bar', stacked=True)
-
-# cabinseries = pd.DataFrame(combineddf['Cabin'])
-# cabinseries_multilevels = combineddf.loc[[88, 97, 118, 311],:]
+#To observe correlation strength.
+combineddf_training_survived = combineddf[combineddf['Train']==1].loc[:, combineddf.columns != 'Train']
+combineddf_training_survived['Survived'] = train_survival
+combineddf_training_survived.corr().to_csv(path_or_buf='Output/correlation.csv', columns=['Survived'])
 
 #Examine 'Age' feature.
 
 fig_age, ax_age = plt.subplots(1, 1, tight_layout=True)
 sns.distplot(combineddf['Age'], bins=15, kde=True, ax=ax_age)
-#ax.hist(combineddf['Age'], bins=10)
-plt.show()
 
-ct = ColumnTransformer([('categorical_normaliser', OneHotEncoder(), ['Pclass', 'Sex', 'Embarked']), ('continuous_normaliser', StandardScaler(), ['Age', 'SibSp', 'Parch', 'Fare'])]) #may not use 'pclass' for OneHotEnc, since values could be ordinal.
-
-ytrain = train_survival
-XTrain_t = ct.fit_transform(traindf)
-XTest_t = ct.fit_transform(testdf)
+# ct = ColumnTransformer([('categorical_normaliser', OneHotEncoder(), ['Pclass', 'Sex', 'Embarked']), ('continuous_normaliser', StandardScaler(), ['Age', 'SibSp', 'Parch', 'Fare'])]) #may not use 'pclass' for OneHotEnc, since values could be ordinal.
+#
+# ytrain = train_survival
+# XTrain_t = ct.fit_transform(traindf)
+# XTest_t = ct.fit_transform(testdf)
 
 '''
 MODEL: DECISION TREE
